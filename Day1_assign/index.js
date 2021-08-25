4f1919c8548974688506f7885e384b019aba0590
Nitin-0205 <75748537+Nitin-0205@users.noreply.github.com>
2021-08-25 20:26:43 +0530

    Add files via upload

---
+++ Day1_assign/index.js
@@ -0,0 +1,10 @@
+a = 10
+b= 5
+sum = a+b;
+mul =a*b;
+div = a/b;
+sub = a-b;
+console.log("Sum of ",a,"and",b,"is : " ,sum);
+console.log("multplication of ",a,"and",b,"is : " ,mul);
+console.log("Division of ",a,"and",b,"is : " ,div);
+console.log("subtraction of ",a,"and",b,"is : " ,sub);
