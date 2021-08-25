9622f6bd8a3e195ad873cc1099b26b9cf510d76b
Nitin-0205 <75748537+Nitin-0205@users.noreply.github.com>
2021-08-25 20:57:53 +0530

    Add files via upload

---
+++ Day2_assign/index.js
@@ -0,0 +1,34 @@
+var Food = [
+    {
+        name: "Burger",
+        price: 100,
+        quantity: 5
+    },
+    {
+        name: "Pizza",
+        price: 200,
+        quantity: 1
+    },
+    {
+        name: "fries",
+        price: 70,
+        quantity: 1
+    },
+    {
+        name: "Cake",
+        price: 170,
+        quantity: 2
+    },
+    {
+        name: "Drink",
+        price: 20,
+        quantity: 7
+    }
+];
+
+
+for(let i =0 ; i <Food.length ;i++){
+    console.log(Food[i].name);
+    console.log(Food[i].price);
+    console.log(Food[i].quantity);
+}
